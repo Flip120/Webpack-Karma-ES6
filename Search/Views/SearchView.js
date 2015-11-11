714187452870f0705defa0add3459458cc6d8166
hooptap-ccabanero <c.cabanerochaparro@gmail.com>
2015-11-11 01:03:56 +0100

    Adding routes v2

--- Search/Views/SearchView.js
+++ Search/Views/SearchView.js
@@ -1,7 +1,26 @@
 import Marionette from 'backbone.marionette';
 
 let SearchView = Marionette.ItemView.extend({
-  template : require('../Template/Search.hbs')
+
+    ui: {
+        "searchBtn": "#btn-search",
+        "randomBtn": "#btn-random"
+    },
+
+    events: {
+        "click @ui.searchBtn": "search",
+        "click @ui.randomBtn": "random"
+    },
+
+    search (){
+        console.log('Search');
+    },
+
+    random (){
+        console.log('Random');
+    },
+
+    template : require('../Template/Search.hbs')
 });
 
 export default SearchView;
